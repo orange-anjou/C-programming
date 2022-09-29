f9af100587be1b8eacfa903958b71c35800638c3
orange-anjou <raphael.anjou@orange.com>
2022-09-29 15:38:20 +0200

    update

--- pointersAndStruct.c
+++ pointersAndStruct.c
@@ -9,13 +9,13 @@ struct person
 
 int main()
 {
-    struct person *personPtr, raphael;
-    personPtr = &raphael;
+	struct person *personPtr, raphael;
+	personPtr = &raphael;
 
 	raphael.age = 20;
-	raphael.weight = 73;
+	raphael.weight = 73.1;
 
 	printf("Age: %d\n", personPtr->age);
-    printf("weight: %f\n", personPtr->weight);
+	printf("weight: %f\n", personPtr->weight);
 	return 0;
 }
